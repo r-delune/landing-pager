8fdb1515018ba5edb3a3bd833542b24110a2e509
Brian Diolún <bdillon@live.ie>
2025-02-13 03:37:17 +0000

    setup nuxt with directus

---
+++ landing-pager/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+const { $directus } = useNuxtApp();
+console.log($directus);
+</script>
---
+++ landing-pager/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{global.title}}</h1>
+        <p>{{global.description}}</p>
+    </div>
+</template>
+
+<script setup>
+const { $directus, $readItems } = useNuxtApp()
+
+const { data: global } = await useAsyncData('global', () => {
+  return $directus.request($readItems('global'))
+})
+</script>
